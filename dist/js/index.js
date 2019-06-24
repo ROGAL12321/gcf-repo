d53325fb40de429b6bdcd51e55b430ecad35d419
Damian Rogulski <damian.rogulski@netinteractive.pl>
2019-06-24 15:26:36 +0200

    xy

---
+++ dist/js/index.js
@@ -0,0 +1,16 @@
+var myAccountHook = document.querySelector('#my-account-hook');
+
+function loadUserData(user) {
+  if (user && user.email) {
+    myAccountHook.innerHTML = `
+      <ul id="kingster-top-bar-menu" class="sf-menu kingster-top-bar-menu kingster-top-bar-right-menu">
+          <li class="menu-item kingster-normal-menu"><a href="/dashboard.html"></a></li>
+      </ul>
+      <div class="kingster-top-bar-right-social"></div>
+      <a class="kingster-top-bar-right-button" href="/dashboard.html">Moje konto</a>
+    `
+  }
+}
+
+
+firebase.auth().onAuthStateChanged(loadUserData);
